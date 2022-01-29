f7903edf9023ac807b0d449261c574004c297d7e
ongunmehmet <ongunmehmet21@gmail.com>
2022-01-29 20:34:04 +0300

    initial commit

--- bookstore-domain/src/main/java/com/example/domain/book/Title.java
+++ bookstore-domain/src/main/java/com/example/domain/book/Title.java
@@ -3,7 +3,7 @@ package com.example.domain.book;
 public final class Title {
     private final String value;
 
-    public Title(String value) {
+    private Title(String value) {
         this.value = value;
     }
 }
