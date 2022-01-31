3329369439f2b106dad096167ec72e6fea812837
ongunmehmet <ongunmehmet21@gmail.com>
2022-02-01 00:13:44 +0300

    Book Event and Book Repository

--- bookstore-domain/src/main/java/com/example/application/BookApplication.java
+++ bookstore-domain/src/main/java/com/example/application/BookApplication.java
@@ -1,14 +1,20 @@
 package com.example.application;
 
+import com.example.domain.book.Author;
 import com.example.domain.book.Book;
 import com.example.domain.book.Isbn;
+import com.example.domain.book.Title;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookApplication {
     Book addBook(Book book);
     Book deleteBook(Isbn isbn);
-    Optional
+    Optional<Book> findBookByIsbn(Isbn isbn);
+    List<Book> findByAuthor(Author author);
+    List<Book> findByTitle(Title title);
+
 
 
 }
--- bookstore-domain/src/main/java/com/example/repository/BookRepository.java
+++ bookstore-domain/src/main/java/com/example/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.example.repository;
 
+import com.example.domain.book.Book;
+import com.example.domain.book.Isbn;
+
 public interface BookRepository {
+    boolean existByIsbn(Isbn isbn);
+
+    Book save(Book book);
 }
