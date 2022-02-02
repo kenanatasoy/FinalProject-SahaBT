59a10bee01928a3cb792035685ecd1363431612c
ongunmehmet <ongunmehmet21@gmail.com>
2022-02-02 23:49:51 +0300

    Catalog service created

--- bookstore-microservice/src/main/java/com/example/repository/BookRepository.java
+++ bookstore-microservice/src/main/java/com/example/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.repository;
 
-public interface BookRepository {
+import com.example.document.BookDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<BookDocument,String> {
 }
