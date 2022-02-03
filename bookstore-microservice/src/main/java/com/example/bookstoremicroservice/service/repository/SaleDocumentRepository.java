88733f63e052b8232315bff6b332d0de9bb79aef
olgun <olgunduman49@gmail.com>
2022-02-04 00:35:30 +0300

    sale commit

--- bookstore-microservice/src/main/java/com/example/bookstoremicroservice/service/repository/SaleDocumentRepository.java
+++ bookstore-microservice/src/main/java/com/example/bookstoremicroservice/service/repository/SaleDocumentRepository.java
@@ -1,7 +1,13 @@
 package com.example.bookstoremicroservice.service.repository;
 
 import com.example.bookstoremicroservice.document.SaleDocument;
+import com.example.sale.domain.Sale;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface SaleDocumentRepository extends MongoRepository<SaleDocument,Integer> {
+    List<Sale> findByBookId(String value);
+
+    List<Sale> findByCustomerId(String value);
 }
