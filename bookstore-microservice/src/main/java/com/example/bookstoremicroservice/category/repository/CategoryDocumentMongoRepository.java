4bb77e2b9d5f4f16b304fbc8c67afcc1aed99ba9
olgun <olgunduman49@gmail.com>
2022-02-07 21:12:24 +0300

    category commit

--- bookstore-microservice/src/main/java/com/example/bookstoremicroservice/category/repository/CategoryDocumentMongoRepository.java
+++ bookstore-microservice/src/main/java/com/example/bookstoremicroservice/category/repository/CategoryDocumentMongoRepository.java
@@ -3,5 +3,5 @@ package com.example.bookstoremicroservice.category.repository;
 import com.example.bookstoremicroservice.category.document.CategoryDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CategoryRepository extends MongoRepository<CategoryDocument,Integer> {
+public interface CategoryDocumentMongoRepository extends MongoRepository<CategoryDocument,Integer> {
 }
--- bookstore-microservice/src/main/java/com/example/bookstoremicroservice/category/service/CategoryService.java
+++ bookstore-microservice/src/main/java/com/example/bookstoremicroservice/category/service/CategoryService.java
@@ -1,16 +1,20 @@
 package com.example.bookstoremicroservice.category.service;
 
+import com.example.bookstoremicroservice.category.dto.request.AddCategoryRequest;
+import com.example.bookstoremicroservice.category.dto.response.AddedCategoryResponse;
 import com.example.bookstoremicroservice.category.dto.response.CategoryResponse;
 import com.example.bookstoremicroservice.category.dto.response.GetCategoryResponse;
 import com.example.domain.category.Category;
-import com.example.dto.response.DeleteCategoryResponse;
+
 
 import java.util.List;
 
 public interface CategoryService {
-    GetCategoryResponse addCategory(Category category);
+  
 
     CategoryResponse getCategoryByCategoryId(int categoryId);
 
     List<CategoryResponse> getAllCatagories();
+
+    AddedCategoryResponse addCategory(AddCategoryRequest request);
 }
