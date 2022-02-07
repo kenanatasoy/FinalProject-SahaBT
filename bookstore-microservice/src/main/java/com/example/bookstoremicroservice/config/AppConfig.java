dc1abc76c79a5dd614d94ea8779a506683f34e30
olgun <olgunduman49@gmail.com>
2022-02-07 15:42:12 +0300

    publisher adapter commit

--- bookstore-microservice/src/main/java/com/example/bookstoremicroservice/config/AppConfig.java
+++ bookstore-microservice/src/main/java/com/example/bookstoremicroservice/config/AppConfig.java
@@ -4,8 +4,12 @@ import com.example.application.CatalogApplication;
 import com.example.application.business.StandardCatalogApplication;
 import com.example.application.business.events.BookEvent;
 import com.example.infrastructure.CatalogEventPublisher;
+import com.example.publisher.application.PublisherApplication;
+import com.example.publisher.application.business.StandardPublisherApplication;
+import com.example.publisher.repository.PublisherRepository;
 import com.example.repository.CatalogRepository;
 import com.example.repository.CategoryRepository;
+import com.example.requisition.application.RequisitionApplication;
 import com.example.sale.application.SaleApplication;
 import com.example.sale.application.business.StandardSaleApplication;
 import com.example.sale.application.business.events.SaleEvent;
@@ -33,6 +37,10 @@ public class AppConfig {
 	@Bean
 	public StockApplication stockApp(StockRepository stockRepository){
 		return new StandardStockApplication(stockRepository);
+	}
+	@Bean
+	public PublisherApplication PublisherApp(SaleEventPublisher<SaleEvent> PublisherEvent, PublisherRepository publisherRepository,RequisitionApplication requisitionApplication) {
+		return new StandardPublisherApplication(PublisherEvent,PublisherRepository,requisitionApplication);
 	}
 
 }
